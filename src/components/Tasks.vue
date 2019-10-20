28502de3317e1344fc7e0451bd9dfeca4d953881
Paul Perkins <paulperkins7@gmail.com>
2019-10-20 17:53:20 -0500

    add Task component and api get call

---
+++ src/components/Tasks.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+        <h1>Tasks</h1>
+        <hr><br><br>
+        <button type="button" class="btn btn-success btn-sm">Add Task</button>
+        <br><br>
+        <table class="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Description</th>
+              <th scope="col">Done?</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(task, index) in tasks" :key="index">
+              <td>{{ task.title }}</td>
+              <td>{{ task.description }}</td>
+              <td>
+                <span v-if="task.done">Yes</span>
+                <span v-else>No</span>
+              </td>
+              <td>
+                <div class="btn-group" role="group">
+                  <button type="button" class="btn btn-warning btn-sm">Complete</button>
+                  <button type="button" class="btn btn-danger px-3 btn-sm">Delete</button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  methods: {
+    getTasks() {
+      const path = 'http://localhost:5000/todo/api/task';
+      axios.get(path)
+        .then((res) => {
+          this.tasks = res.data.result;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    },
+  },
+  created() {
+    this.getTasks();
+  },
+};
+</script>
