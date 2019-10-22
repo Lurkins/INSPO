643170edc0e6bfc57633b351206fc2218dbd4a70
Paul Perkins <paulperkins7@gmail.com>
2019-10-21 23:19:58 -0500

    add photo upload frontend

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -24,6 +24,21 @@
                 <span v-else>No</span>
               </td>
               <td>
+            <div class="large-12 medium-12 small-12 cell">
+      <label>File
+        <input type="file" id="file" ref="file" v-on:change="handleFileUpload()"/>
+      </label>
+        <button v-on:click="submitFile()">Submit</button>
+    </div>
+<!-- Styled -->
+    <!-- <b-form-file
+      v-model="file"
+      :state="Boolean(file)"
+      placeholder="Choose a file or drop it here..."
+      drop-placeholder="Drop file here..."
+      accept=".jpg, .png, .gif"
+    ></b-form-file>
+    <div class="mt-3">Selected file: {{ file ? file.name : '' }}</div> -->
                 <div class="btn-group" role="group">
                   <button type="button" class="btn btn-warning btn-sm">Complete</button>
                   <button type="button" class="btn btn-danger px-3 btn-sm">Delete</button>
@@ -44,6 +59,7 @@ export default {
   data() {
     return {
       tasks: [],
+      file: null,
     };
   },
   methods: {
@@ -57,6 +73,28 @@ export default {
           // eslint-disable-next-line
           console.error(error);
         });
+    },
+    submitFile() {
+      const formData = new FormData();
+      formData.append('file', this.file);
+      const path = 'http://localhost:5000/todo/api/photo';
+      axios.post(path,
+        formData,
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        })
+        .then((res) => {
+          console.log('SUCCESS!!', res);
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.log('FAILURE!!', error);
+        });
+    },
+    handleFileUpload() {
+      this.file = this.$refs.file.files[0];
     },
   },
   created() {
