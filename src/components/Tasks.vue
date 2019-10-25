9ee0ddd89532b8d0fa3d57e9b9123f1d01cfecbb
Paul Perkins <paulperkins7@gmail.com>
2019-10-24 23:05:26 -0500

    add new task modal

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -6,7 +6,7 @@
         <img v-if="image" v-bind:src="`http://localhost:5000/file/${image}`" alt="${image}">
         <div v-else></div>
         <hr><br><br>
-        <button type="button" class="btn btn-success btn-sm">Add Task</button>
+        <button type="button" class="btn btn-info btn-sm" v-b-modal.task-modal>Add Task</button>
         <br><br>
         <table class="table table-hover">
           <thead>
@@ -30,9 +30,13 @@
                 <button
                   v-on:click="handleComplete(task._id.$oid)"
                   type="button"
-                  class="btn btn-warning btn-sm"
+                  class="btn btn-success btn-sm"
                 >Complete</button>
-                  <button type="button" class="btn btn-danger px-3 btn-sm">Delete</button>
+                <button
+                  type="button"
+                  class="btn btn-secondary px-3 btn-sm"
+                  v-on:click="handleDelete(task._id.$oid)"
+                >Delete</button>
                 </div>
               </td>
             </tr>
@@ -46,6 +50,37 @@
           <button v-on:click="submitFile()">Submit</button>
       </div>
     </div>
+      <b-modal ref="addTaskModal"
+        id="task-modal"
+        title="Add a new task"
+        hide-footer>
+        <b-form @submit="onSubmit" @reset="onReset" class="w-100">
+          <b-form-group id="form-title-group"
+            label="Task:"
+            label-for="form-title-input">
+          <b-form-input id="form-title-input"
+            type="text"
+            v-model="addTaskForm.title"
+            required
+            placeholder="Enter task">
+          </b-form-input>
+          </b-form-group>
+          <b-form-group id="form-description-group"
+            label="Description:"
+            label-for="form-description-input">
+          <b-form-input id="form-description-input"
+            type="text"
+            v-model="addTaskForm.description"
+            required
+            placeholder="Enter description">
+          </b-form-input>
+          </b-form-group>
+        <b-form-group id="form-read-group">
+        </b-form-group>
+        <b-button class="mr-3" type="submit" variant="primary">Submit</b-button>
+        <b-button type="reset" variant="danger">Reset</b-button>
+      </b-form>
+    </b-modal>
   </div>
 </template>
 
@@ -58,6 +93,11 @@ export default {
       tasks: [],
       file: null,
       image: false,
+      addTaskForm: {
+        title: '',
+        description: '',
+        done: [],
+      },
     };
   },
   methods: {
@@ -109,6 +149,53 @@ export default {
           // eslint-disable-next-line
           console.error(error);
         });
+    },
+    handleDelete(id) {
+      // const name = this.tasks[0].title;
+      const path = `http://localhost:5000/todo/api/task/delete/${id}`;
+      axios.delete(path)
+        .then((res) => {
+          this.tasks = res.data;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    },
+    addTask(payload) {
+      const path = 'http://localhost:5000/todo/api/task';
+      axios.post(path, payload)
+        .then(() => {
+          this.getTasks();
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.log(error);
+          this.getTasks();
+        });
+    },
+    initForm() {
+      this.addTaskForm.title = '';
+      this.addTaskForm.description = '';
+      this.addTaskForm.read = [];
+    },
+    onSubmit(evt) {
+      evt.preventDefault();
+      this.$refs.addTaskModal.hide();
+      let done = false;
+      if (this.addTaskForm.done[0]) done = true;
+      const payload = {
+        title: this.addTaskForm.title,
+        description: this.addTaskForm.description,
+        done, // property shorthand
+      };
+      this.addTask(payload);
+      this.initForm();
+    },
+    onReset(evt) {
+      evt.preventDefault();
+      this.$refs.addTaskModal.hide();
+      this.initForm();
     },
   },
   created() {
