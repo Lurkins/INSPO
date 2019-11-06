97f4bcfbc3262e545a40842a3b1f1eb1c5999164
Paul Perkins <paulperkins7@gmail.com>
2019-11-05 23:21:52 -0600

    connect bootstrap file input and f ix styles, add footer

--- src/components/EditItem.vue
+++ src/components/EditItem.vue
@@ -1,59 +1,86 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-sm-12">
-        <h1>Edit Task</h1>
-        <hr><br><br>
-        <br><br>
-        <div>
-          <div>
-            <b-img :src="`http://localhost:5000/file/${image}`" fluid alt="task"></b-img>
-              <b-form-group label="image:" label-for="file-default" label-cols-sm="2">
-                <b-form-file id="file" v-on:change="handleFileUpload()"></b-form-file>
-              </b-form-group>
-            <!-- <div class="large-12 medium-12 small-12">
-              <label>File
-               <input type="file" id="file" ref="file" v-on:change="handleFileUpload()"/>
-              </label>
-              <button v-on:click="submitFile(id)">Submit</button>
-            </div> -->
-          </div>
-          <div>{{ title }}</div>
-          <div>{{ description }}</div>
+<div>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <h1>{{ title }}</h1>
+                <hr>
+                <p>{{ description }}</p>
+            </div>
         </div>
-        <div>
-            <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-            <b-form-group
-                id="input-group-1"
-                label="Title:"
-                label-for="input-1"
-            >
-                <b-form-input
-                id="input-1"
-                v-model="form.title"
-                type="text"
-                required
-                placeholder="Enter title"
-                ></b-form-input>
-            </b-form-group>
-
-            <b-form-group id="input-group-2" label="Description:" label-for="input-2">
-                <b-form-input
-                id="input-2"
-                v-model="form.description"
-                required
-                placeholder="Enter description"
-                ></b-form-input>
-            </b-form-group>
-            <b-button type="submit" variant="primary">Submit</b-button>
-            <b-button type="reset" variant="danger">Reset</b-button>
-            </b-form>
-            <b-card class="mt-3" header="Form Data Result">
-            <pre class="m-0">{{ form }}</pre>
-            </b-card>
-        </div>
-      </div>
     </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <b-img :src="`http://localhost:5000/file/${image}`" alt="task" fluid class="mb-5"></b-img>
+            </div>
+            <div class="col-md-6">
+                <div>
+                    <div>
+                        <b-form-group label="Image:" label-for="file" label-cols-sm="12">
+                            <b-form-file id="file" v-model="file" class="mb-5">
+                            </b-form-file>
+                            <button
+                                v-on:click="submitFile(id)"
+                                type="button"
+                                class="btn btn-primary px-3">Submit
+                            </button>
+                        </b-form-group>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <hr>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="my-5">
+                    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+                        <b-form-group
+                            id="input-group-1"
+                            label="Title:"
+                            label-for="input-1"
+                        >
+                            <b-form-input
+                            id="input-1"
+                            v-model="form.title"
+                            required
+                            placeholder="Enter title"
+                            ></b-form-input>
+                        </b-form-group>
+                        <b-form-group id="input-group-2" label="Description:" label-for="input-2">
+                            <b-form-input
+                            id="input-2"
+                            v-model="form.description"
+                            placeholder="Enter description"
+                            required
+                            ></b-form-input>
+                        </b-form-group>
+                        <b-button type="submit" variant="primary">Save</b-button>
+                        <b-button type="reset" variant="danger">Reset</b-button>
+                    </b-form>
+                    <b-card class="my-5 mb-5" header="Form Data Result">
+                    <pre class="m-0">{{ form }}</pre>
+                    </b-card>
+                </div>
+            </div>
+        </div>
+    </div>
+        <div class="container">
+            <hr>
+        </div>
+        <footer>
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="p-5">
+                        <h6 class="text-dark my-5 py-5">Footer - {{ title }}</h6>
+                    </div>
+                </div>
+            </div>
+        </footer>
   </div>
 </template>
 
@@ -112,10 +139,10 @@ export default {
           console.log('FAILURE!!', error);
         });
     },
-    handleFileUpload() {
-      // eslint-disable-next-line
-      this.file = this.$refs.file.files[0];
-    },
+    // handleFileUpload() {
+    //   // eslint-disable-next-line
+    //   this.file = this.$refs.file.files[0];
+    // },
     onSubmit(evt) {
       evt.preventDefault();
       const path = `http://localhost:5000/todo/api/task/edit/${this.id}`;
@@ -127,6 +154,7 @@ export default {
         .then((res) => {
           this.title = res.data.result.title;
           this.description = res.data.result.description;
+          this.onReset(evt);
         })
         .catch((error) => {
           // eslint-disable-next-line
